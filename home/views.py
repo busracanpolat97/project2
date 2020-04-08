9f694443e09a529d9bee96f17d92192425432506
busrasosun <ssnbsra@gmail.com>
2020-04-09 01:47:05 +0300

    hakkımızda referanslar iletişim sayfaları eklendi

--- home/views.py
+++ home/views.py
@@ -9,3 +9,18 @@ def index(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting}
     return render(request, 'index.html', context)
+
+def hakkimizda(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'hakkimizda'}
+    return render(request, 'hakkimizda.html', context)
+
+def referanslar(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'referanslar'}
+    return render(request, 'referanslarimiz.html', context)
+
+def iletisim(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'iletisim'}
+    return render(request, 'iletisim.html', context)
